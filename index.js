7df5b4e6a104b2e982f13b05731e1305d8097777
Alexaiscoding <93916126+Alexaiscoding@users.noreply.github.com>
2022-01-11 20:13:57 +0100

    add

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
+const { insertMany, updateMany, updateOne } = require('./models/Recipe.model');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
@@ -15,9 +16,37 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
+
   .then(() => {
     // Run your code here, after you have insured that the connection was made
-  })
+    const pizza = {
+      title: "Pizza 4 fromaggi",
+      ingredients: [
+        "Gorgonzola",
+        "Mozarella",
+        "Pecorino",
+        "Emmental"
+      ],
+      cuisine: "Italian",
+      creator: "Alexandra"
+    }
+
+    Recipe.create({title:"pizza 4 fromaggi", cuisine: "Italian"})
+      .then(createdRecipe => console.log("The new recipe has been created",createdRecipe.title))
+      .catch(error => console.log("An error happened while saving a new recipe",error))
+
+    Recipe.insertMany ()
+    .then(inserManyRecipe => console.log ("inserted recipe",inserManyRecipe.title))
+    .catch(error => console.log("An error happened while saving",inserManyRecipe.title),error)
+
+    Recipe.findOneAndUpdate({duration= 100})
+    .then(findOneAndYpdateRecipe => console.log ("Updated recipe",findOneAndYpdateRecipe .title))
+    .catch(error => console.log("An error happened while Updating",findOneAndYpdateRecipe.title,error))
+
+    
+
   .catch(error => {
     console.error('Error connecting to the database', error);
+  })
   });
+
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,9 +1,34 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const data= require('../data.json')
+
+// connect to the databasse:
+
+mongoose.connect('mongodb://localhost/recipeApp'
+  .then(() => { console.log('Connected to Mongo!')
+  .catch((err) => { console.log('An error happened:', err) }
+  )
+
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+
+    title: {type: String, required: true, unique: true},
+    level: {type: String, enum:[ "Easy Peasy","Animateur Chef","UltraPro Chef" ]},
+
+    ingredients: [String],
+    cuisine:{type: String, required: true},
+    dishType:{type:String,enum: ["breakfast","main_course","soup","snack"]},
+    image:{type:String, default:"https://images.media-allrecipes.com/images/75131.jpg"},
+    duration: {type: Number, min:0},
+    creator: {type:String},
+    created: {type:Date ,default: Date.now}
+
 });
+
+
+
+
+
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
 
